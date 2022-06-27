533ef4c8f7f87add4dd7e6cc332ae66fb08f189b
Martin <nitromagix@gmail.com>
2022-06-26 18:16:41 -0700

    changed useState counter to an object

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import './App.css';
+import "./App.css";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import trace from './helper'
 
 function App() {
-   let [counter, changeCounter] = useState(1);
+  let [obj, updateObj] = useState({ title: "count", counter: 1 });
 
-   useEffect(() => {
-      console.log('useEffect')
-      document.title = counter;
-   }, [counter])
+  useEffect(() => {
+    console.log("component");
+    document.title = `${obj.title} - ${obj.counter}`;
 
-   window.changeCounter = changeCounter;
-   return (
-      <div>
-         <h1>{counter}</h1>
-         <h3>{counter}</h3>
-         <h5>{counter}</h5>
-         <button onClick={() => changeCounter(counter += 1)} >Update Counter</button>
-      </div>
-   );
+    return () => {
+      console.log("component unmounted");
+    };
+  }, [obj]);
+
+  const incrementCounter = () => {
+    updateObj((obj) => ({
+      ...obj,
+      counter: obj.counter + 1,
+    }));
+    trace('previous obj')(obj);
+  };
+
+  // window.changeCounter = updateObj;
+  return (
+    <div>
+      <h1>{obj.counter}</h1>
+      <h3>{obj.counter}</h3>
+      <h5>{obj.counter}</h5>
+      <button onClick={incrementCounter}>Update Counter</button>
+    </div>
+  );
 }
 
 export default App;
